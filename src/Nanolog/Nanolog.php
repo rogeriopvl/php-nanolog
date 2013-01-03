d562f570cae1067051579aad47245c6cbe5514b9
rogeriopvl <rogeriopvl@gmail.com>
2013-01-03 18:00:22 +0000

    Added more code, and tests. All passing.

--- src/Nanolog/Nanolog.php
+++ src/Nanolog/Nanolog.php
@@ -17,14 +17,10 @@ class Nanolog
     const INFO = 3;
     const DEBUG = 4;
 
-    const OPEN = 1;
-    const FAILED = 2;
-    const CLOSED = 3;
-
-    private $_status;
     private $_filePath;
     private $_handle;
     private $_name;
+    private $_level;
 
     private static $_instances;
 
@@ -36,19 +32,23 @@ class Nanolog
      * @param String $fileName;
      * @param String $name
      */
-    public function __construct($level, $folder, $name = null, $fileName = null)
+    private function __construct($level, $folder, $name = null, $fileName = null)
     {
         if (!is_dir($folder) || !is_writable($folder)) {
             throw new \Exception('Folder does not exist, or is not writable');
         }
 
         self::$_instances = array();
-        $this->_status = self::CLOSED;
         $this->_name = $name;
+        $this->_level = (int)$level;
         $this->_filePath = $folder . DIRECTORY_SEPARATOR;
 
         $this->_filePath .= $fileName === null ? date('Y-m-d').'.log' : $fileName;
+        $this->_handle = fopen($this->_filePath, 'a');
 
+        if (!$this->_handle) {
+            throw new \Exception('Error opening log file with path: ' . $this->_filePath);
+        }
     }
 
     /**
@@ -59,5 +59,83 @@ class Nanolog
         if ($this->_handle) {
             fclose($this->_handle);
         }
+    }
+
+    /**
+     * Returns a log instance
+     *
+     * @param String $name, the name of the logger
+     * @return mixed Nanolog instance or false if no instance matches the name
+     */
+    public static function getInstance($name = null)
+    {
+        if (isset(self::$_instances[$name])) {
+            return self::$_instances[$name];
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Creates a new Nanolog instance
+     */
+    public static function create($level, $folder, $name = null, $fileName = null)
+    {
+        if ($name !== null) {
+            if (isset(self::$_instances[$name])) {
+                return false; // should we return the instance instead?
+            }
+        } else {
+            // sorry you can't have more than one unamed instance :P
+            if (isset(self::$_instances[$name])) {
+                return false;
+            }
+        }
+        self::$_instances[$name] = new Nanolog($level, $folder, $name, $fileName);
+
+        return self::$_instances[$name];
+    }
+
+    public function log($message, $level)
+    {
+        $linePrefix = date('Y-m-d H:i:s').' ';
+        $message = $linePrefix.$message."\n";
+        // we cant throw here an exception if its not able to write
+        // because its not practical to use try-catch everytime we log
+        fwrite($this->_handle, $message);
+    }
+
+    public function critical($message)
+    {
+        $this->log('CRITICAL '.$message, self::CRITICAL);
+    }
+
+    public function error($message)
+    {
+        $this->log('ERROR '.$message, self::ERROR);
+    }
+
+    public function warning($message)
+    {
+        $this->log('WARNING '.$message, self::WARNING);
+    }
+
+    public function info($message)
+    {
+        $this->log('INFO '.$message, self::INFO);
+    }
+
+    public function debug($message)
+    {
+        $this->log('DEBUG '.$message, self::DEBUG);
+    }
+
+    /**
+     * Get the name of the instance
+     *
+     * @return String the name of the instance
+     */
+    public function getName() {
+        return $this->_name;
     }
 }
--- test/NanologTest.php
+++ test/NanologTest.php
@@ -13,7 +13,7 @@ class NanologTest extends PHPUnit_Framework_TestCase
      */
     public function testThrowsExceptionWhenFolderDoesNotExist()
     {
-        $log = new \Nanolog\Nanolog(\Nanolog\Nanolog::DEBUG, '/xpto/foo');
+        \Nanolog\Nanolog::create(\Nanolog\Nanolog::DEBUG, '/xpto/foo');
     }
 
     /**
@@ -21,12 +21,117 @@ class NanologTest extends PHPUnit_Framework_TestCase
      */
     public function testThrowsExceptionWhenFolderDoesHaveWritePermissions()
     {
-        $log = new \Nanolog\Nanolog(\Nanolog\Nanolog::DEBUG, '/var');
+        \Nanolog\Nanolog::create(\Nanolog\Nanolog::DEBUG, '/var');
     }
 
-    public function testIsClosedOnStart()
+    public function testCreateAddsAnInstance()
     {
-        $log = new \Nanolog\Nanolog(\Nanolog\Nanolog::DEBUG, '/tmp');
-        $this->assertEquals(PHPUnit_Framework_Assert::readAttribute($log, '_status'), \Nanolog\Nanolog::CLOSED);
+        $log = \Nanolog\Nanolog::create(\Nanolog\Nanolog::DEBUG, '/tmp');
+        $this->assertInstanceOf('\Nanolog\Nanolog', $log);
+    }
+
+    /**
+     * @depends testCreateAddsAnInstance
+     */
+    public function testCantCreateMoreThanOneAnonymousInstance()
+    {
+        $log = \Nanolog\Nanolog::create(\Nanolog\Nanolog::DEBUG, '/tmp');
+        $log2 = \Nanolog\Nanolog::create(\Nanolog\Nanolog::DEBUG, '/tmp');
+
+        $this->assertEquals(false, $log2);
+    }
+
+    /**
+     * @depends testCreateAddsAnInstance
+     */
+    public function testCreateNamedInstance()
+    {
+        $log = \Nanolog\Nanolog::create(\Nanolog\Nanolog::DEBUG, '/tmp', 'log1');
+
+        $this->assertInstanceOf('\Nanolog\Nanolog', $log);
+        $this->assertEquals('log1', $log->getName());
+    }
+
+    /**
+     * @depends testCreateAddsAnInstance
+     * @depends testCreateNamedInstance
+     */
+    public function testCreateMultipleNamedInstances()
+    {
+        $log1 = \Nanolog\Nanolog::create(\Nanolog\Nanolog::DEBUG, '/tmp', 'log_1');
+        $log2 = \Nanolog\Nanolog::create(\Nanolog\Nanolog::INFO, '/tmp', 'log_2');
+
+        $this->assertEquals('log_1', $log1->getName());
+        $this->assertEquals('log_2', $log2->getName());
+    }
+
+    /**
+     * @depends testCreateAddsAnInstance
+     * @depends testCreateNamedInstance
+     */
+    public function testGetAnonymousInstance()
+    {
+        $log = \Nanolog\Nanolog::create(\Nanolog\Nanolog::DEBUG, '/tmp');
+        $logInstance = \Nanolog\Nanolog::getInstance();
+
+        $this->assertInstanceOf('\Nanolog\Nanolog', $logInstance);
+        $this->assertEquals(null, $logInstance->getName());
+    }
+
+    /**
+     * @depends testCreateAddsAnInstance
+     * @depends testCreateNamedInstance
+     */
+    public function testGetNamedInstance()
+    {
+        $log = \Nanolog\Nanolog::create(\Nanolog\Nanolog::DEBUG, '/tmp', 'test1');
+        $logInstance = \Nanolog\Nanolog::getInstance('test1');
+
+        $this->assertInstanceOf('\Nanolog\Nanolog', $logInstance);
+        $this->assertEquals('test1', $logInstance->getName());
+
+    }
+
+    public function testLogCritical()
+    {
+        $log = \Nanolog\Nanolog::create(\Nanolog\Nanolog::DEBUG, '/tmp', 'test2');
+        $log->critical('Disk is full');
+    }
+
+    /**
+     * @depends testLogCritical
+     */
+    public function testLogError()
+    {
+        $log = \Nanolog\Nanolog::getInstance('test2');
+        $log->error('Could not write message');
+
+    }
+
+    /**
+     * @depends testLogCritical
+     */
+    public function testLogWarning()
+    {
+        $log = \Nanolog\Nanolog::getInstance('test2');
+        $log->warning('Database is taking too long to respond');
+    }
+
+    /**
+     * @depends testLogCritical
+     */
+    public function testLogInfo()
+    {
+        $log = \Nanolog\Nanolog::getInstance('test2');
+        $log->info('User has logged out');
+    }
+
+    /**
+     * @depends testLogCritical
+     */
+    public function testLogDebug()
+    {
+        $log = \Nanolog\Nanolog::getInstance('test2');
+        $log->debug(print_r($log, true));
     }
 }
